faae91da60210ec6eff78b8e84a0bf37f087b875
Simba <isarawadano1@gmail.com>
2023-03-10 11:33:14 +0200

    uploader view

--- resources/views/livewire/uploader.blade.php
+++ resources/views/livewire/uploader.blade.php
@@ -1,5 +1,19 @@
 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
     <div class="p-6 text-gray-900">
-        
+        <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-dashed rounded-md">
+            <div class="space-y-1 text-center">
+                svg
+                <div class="flex text-sm text-gray-600">
+                    <label for="file-upload"
+                        class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                        <span>Upload a file</span>
+                        <input id="file-upload" name="file-upload" type="file" class="sr-only" multiple>
+                    </label>
+                    <p class="pl-1">or drag and drop</p>
+                </div>
+                <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+            </div>
+        </div>
+        <p class="mt-2 text-red-500 text-sm">Some validation error</p>
     </div>
 </div>
